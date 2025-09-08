dd1fb5c2214b253016d9669754500a1918113756
Vrooni <laura.adler@gmx.de>
2025-09-08 16:16:06 +0200

    constants lessons

---
+++ constants/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Lesson 267</title>
+</head>
+
+<body>
+  <?php
+  const HI = "hi";
+  // HI = "hi";
+  ?>
+</body>
+
+</html>
