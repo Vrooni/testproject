72ce11ba2053258d5dd4875e48d33c8da6b0b069
Vrooni <laura.adler@gmx.de>
2025-11-28 11:07:16 +0100

    register forumlar lessons

---
+++ formulars/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once("init.php");
+
+if (isset($_SERVER["PATH_INFO"])) {
+  $request = $_SERVER["PATH_INFO"];
+} else {
+  $request = $_SERVER["REQUEST_URI"];
+}
+
+$router = $container->build("router");
+
+if ($request == "/testproject/formulars/") {
+  $router->add("home_controller", "home");
+} elseif ($request == "/Users") {
+  $router->add("user_controller", "index");
+} elseif ($request == "/User=user") {
+  $router->add("user_controller", "profile");
+} elseif ($request == "/Register") {
+  $router->add("register_controller", "register");
+} else {
+  $router->add("error_controller", "error");
+}
---
+++ formulars/src/app/Container.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\app;
+
+use App\user\UserDatabase;
+use App\connections\MySQL;
+use App\home\mvc\HomeController;
+use App\error\mvc\ErrorController;
+use App\home\HomeDatabase;
+use App\register\mvc\RegisterController;
+use App\user\mvc\UserController;
+
+class Container
+{
+  private $class_instances = [];
+  private $builds = [];
+
+  public function __construct()
+  {
+    $this->builds = [
+      "router" => function () {
+        return new Router($this->build('container'));
+      },
+
+      "container" => function () {
+        return new Container();
+      },
+
+      "user_data_base" => function () {
+        return new UserDatabase($this->build('pdo'));
+      },
+
+      "home_data_base" => function () {
+        return new HomeDatabase($this->build('pdo'));
+      },
+
+      "user_controller" => function () {
+        return new UserController($this->build('user_data_base'));
+      },
+
+      "register_controller" => function () {
+        return new RegisterController($this->build('user_data_base'));
+      },
+
+      "home_controller" => function () {
+        return new HomeController($this->build('home_data_base'));
+      },
+
+      "error_controller" => function () {
+        return new ErrorController();
+      },
+
+      "pdo" => function () {
+        $connection = new MySQL();
+        return $connection->connect_my_sql();
+      }
+    ];
+  }
+
+  public function build($object)
+  {
+    if (isset($this->builds[$object])) {
+      if (empty($this->class_instances[$object])) {
+        $this->class_instances[$object] = $this->builds[$object]();
+      }
+    }
+
+    return $this->class_instances[$object];
+  }
+}
